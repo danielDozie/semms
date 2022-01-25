69063f573b033235f6ae17f339a237f5511a631b
badt0men <placmedia@gmail.com>
2022-01-25 03:35:41 +0100

    .....

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env:{
+        S_TOKEN: process.env.NEXT_PUBLIC_S_TOKEN,
+    },
+    images: {
+        domains: ['semmslux.com', 'cdn.shopify.com'],
+      },
+    basePath: '',
+  }
