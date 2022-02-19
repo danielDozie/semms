ed277d9ef4777f082f2b8a4ac9ad3b1c6ccaab7a
badt0men <placmedia@gmail.com>
2022-02-19 21:06:00 +0100

    ....

--- src/utils/apolloClient.tsx
+++ src/utils/apolloClient.tsx
@@ -6,7 +6,7 @@ import {
 
 import { setContext } from "@apollo/client/link/context";
 
-const httpLink = createHttpLink({ uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT});
+const httpLink = createHttpLink({ uri: 'https://semmslux.myshopify.com/api/graphql'});
 
 const middlewareLink = setContext(() => ({
   headers: {
