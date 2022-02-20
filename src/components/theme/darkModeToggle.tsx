69e1c537fc781a04d25da85f5a5e801eb78c4c3f
badt0men <placmedia@gmail.com>
2022-02-21 00:01:34 +0100

    fixed the switch

--- src/components/theme/darkModeToggle.tsx
+++ src/components/theme/darkModeToggle.tsx
@@ -4,18 +4,25 @@ import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 
 const Toggle = () => {
     const { theme, setTheme }: any = React.useContext(ThemeContext);
+    const [browserTheme, setBrowserTheme] = React.useState();
+    
+    //causing hydration error... so I had to do this to ensure the website default theme on visitors device is loaded first.
+    React.useEffect(() =>{
+        setBrowserTheme(theme);
+    }, [theme, browserTheme])
+    
     return (
-        <div className="flex cursor-pointer" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-            {theme ? (<>
+        <div className="flex cursor-pointer drop-shadow-lg" onClick={() => setTheme(browserTheme === 'dark' ? 'light' : 'dark')}>
+            {browserTheme === 'dark' ? (<>
                 <BsFillSunFill size={15}
-                    className="text-gold dark:text-gold text-lg mt-1 mr-2 || transition duration-500 ease-in-out"
+                    className="text-gold dark:text-gold text-lg mt-1 mr-1.5 || transition duration-500 ease-in-out"
                 />
-                <p className="text-[9px] font-light mt-.5 text-gray-400">Light Mode</p>
+                <p className="text-[9px] font-bold mt-.5 text-gray-400 italic">Light Mode</p>
             </>) : (<>
                 <BsFillMoonStarsFill size={14}
-                    className="text-black dark:text-black text-lg cursor-pointer mt-1 mr-2 || transition duration-500 ease-in-out"
+                    className="text-black dark:text-black text-lg cursor-pointer mt-1 mr-1.5 || transition duration-500 ease-in-out"
                 />
-                <p className="text-[9px] font-light mt-.5 text-gray-600 ">Dark Mode</p>
+                <p className="text-[9px] font-bold mt-.5 text-gray-600 italic">Dark Mode</p>
             </>)
             }
         </div>
